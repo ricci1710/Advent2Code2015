481f99e127ec70cdb44ba45ba16e698f77f89601
Rico Kunz <ricci1710@gmail.com>
2022-12-29 08:54:27 +0100

    day 04 start

--- src/day04/Day04.test.js
+++ src/day04/Day04.test.js
@@ -1,35 +1,35 @@
-import DayXX from "./DayXX";
-import {MOCK_DEMO_DATA_DAY_XX} from "./DemoDataXX";
-import {MOCK_LIFE_DATA_DAY_XX} from "./LifeDataXX";
+import Day04 from "./Day04";
+import {MOCK_DEMO_DATA_DAY_04} from "./DemoData04";
+import {MOCK_LIFE_DATA_DAY_04} from "./LifeData04";
 
 describe('Test Class DayXX', () => {
   test('constructor', () => {
-    const day = new DayXX();
+    const day = new Day04();
     expect(day).toBeDefined();
   });
   test('calcPartOne with demo data', () => {
-    const storeData = MOCK_DEMO_DATA_DAY_XX.split('\n');
-    const day = new DayXX(storeData);
+    const storeData = MOCK_DEMO_DATA_DAY_04.split('\n');
+    const day = new Day04(storeData);
     const result = day.calcPartOne();
     expect(result).toEqual(-1);
   });
   test('calcPartOne with life data', () => {
-    const storeData = MOCK_LIFE_DATA_DAY_XX.split('\n');
-    const day = new DayXX(storeData);
+    const storeData = MOCK_LIFE_DATA_DAY_04.split('\n');
+    const day = new Day04(storeData);
     const result = day.calcPartOne();
     expect(result).toEqual(-1);
   });
   test('calcPartTwo with demo data', () => {
-    const storeData = MOCK_DEMO_DATA_DAY_XX.split('\n');
-    const day = new DayXX(storeData);
+    const storeData = MOCK_DEMO_DATA_DAY_04.split('\n');
+    const day = new Day04(storeData);
     const result = day.calcPartTwo();
     expect(result).toEqual(-1);
   });
   test('calcPartTwo with life data', () => {
-    const storeData = MOCK_LIFE_DATA_DAY_XX.split('\n');
-    const day = new DayXX(storeData);
+    const storeData = MOCK_LIFE_DATA_DAY_04.split('\n');
+    const day = new Day04(storeData);
     const result = day.calcPartTwo();
-    // You have completed Day XX!
+    // You have completed Day 04!
     expect(result).toEqual(-1);
   });
 });
---
+++ src/day04/day04_de.md
@@ -0,0 +1,21 @@
+**--- Tag 4: Das ideale Geschenk für die Strümpfe ---**
+
+Der Weihnachtsmann braucht Hilfe beim Schürfen von AdventCoins (sehr ähnlich wie Bitcoins), die er als Geschenke für
+alle wirtschaftlich vorausschauenden Mädchen und Jungen verwenden kann.
+
+Dazu muss er MD5-Hashes finden, die im Hexadezimalsystem mit mindestens fünf Nullen beginnen. Die Eingabe für den
+MD5-Hash ist ein geheimer Schlüssel (Ihre Rätseleingabe, siehe unten), gefolgt von einer Zahl in Dezimalzahlen. Um
+AdventCoins abzubauen, müssen Sie Santa die niedrigste positive Zahl (ohne führende Nullen: 1, 2, 3, ...) finden, die
+einen solchen Hash erzeugt.
+
+```
+Zum Beispiel:
+
+Wenn Ihr geheimer Schlüssel abcdef ist, ist die Antwort 609043, weil der MD5-Hash von abcdef609043 mit fünf Nullen 
+beginnt (000001dbbfa...), und es ist die niedrigste Zahl, die das tut.
+
+Wenn Ihr geheimer Schlüssel pqrstuv ist, ist die niedrigste Zahl, mit der er sich zu einem MD5-Hash kombinieren lässt,
+der mit fünf Nullen beginnt, 1048970; das heißt, der MD5-Hash von pqrstuv1048970 sieht aus wie 000006136ef....
+```
+
+Ihre Rätseleingabe lautet **bgvyzdsv**.
