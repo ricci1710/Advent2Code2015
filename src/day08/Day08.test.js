a957a31ed729eb78b6845da32c88c00c7431745a
Rico Kunz <rico.kunz@leutek.de>
2023-01-04 16:57:41 +0100

    day 8 start

--- src/day08/Day08.test.js
+++ src/day08/Day08.test.js
@@ -47,6 +47,22 @@ describe('Test Class Day08', () => {
     const asciiLength = Day08.getAsciiLength(data);
     expect(asciiLength).toEqual(32);
   });
+  test('"ogvazaqy\"j\x73"', () => {
+    const storeData = fs.readFileSync('./src/day08/DemoData08.bin', 'ascii').split('\r\n');
+    const data = storeData[10];
+    const utf8Length = Day08.getUtf8Length(data);
+    expect(utf8Length).toEqual(17);
+    const asciiLength = Day08.getAsciiLength(data);
+    expect(asciiLength).toEqual(11);
+  });
+  test('"\\\xb1qso\"s"', () => {
+    const storeData = fs.readFileSync('./src/day08/DemoData08.bin', 'ascii').split('\r\n');
+    const data = storeData[11];
+    const utf8Length = Day08.getUtf8Length(data);
+    expect(utf8Length).toEqual(14);
+    const asciiLength = Day08.getAsciiLength(data);
+    expect(asciiLength).toEqual(7);
+  });
   test('calcPartOne with demo data', () => {
     const storeData = fs.readFileSync('./src/day08/DemoData08.bin', 'ascii').split('\r\n').slice(0, 4);
     const day = new Day08(storeData);
