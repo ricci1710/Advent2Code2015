ecc8af95233963f87a9656f7e8d3106d2c75c32d
ricci1710 <ricci1710@gmail.com>
2023-01-06 12:52:05 +0100

    day 6 start

--- src/day09/Day09.test.js
+++ src/day09/Day09.test.js
@@ -1,26 +1,26 @@
-import DayXX from "./DayXX";
+import Day09 from "./Day09";
 
-describe('Test Class DayXX', () => {
-  const demoDay = new DayXX(-1, true);
-  const lifeDay = new DayXX(-1, false);
+describe('Test Class Day09', () => {
+  const demoDay = new Day09(-1, true);
+  const lifeDay = new Day09(-1, false);
 
   test('constructor', () => {
     expect(demoDay).toBeDefined();
     expect(lifeDay).toBeDefined();
   });
-  test('calcPartOne with demo data XX', () => {
+  test('calcPartOne with demo data', () => {
     const result = demoDay.calcPartOne();
     expect(result).toEqual(-1);
   });
-  test('calcPartOne with life data XX', () => {
+  test('calcPartOne with life data', () => {
     const result = lifeDay.calcPartOne();
     expect(result).toEqual(-1);
   });
-  test('calcPartTwo with demo data XX', () => {
+  test('calcPartTwo with demo data', () => {
     const result = demoDay.calcPartTwo();
     expect(result).toEqual(-1);
   });
-  test('calcPartTwo with life data XX', () => {
+  test('calcPartTwo with life data', () => {
     const result = lifeDay.calcPartTwo();
     // You have completed Day XX!
     expect(result).toEqual(-1);
---
+++ src/day09/day09_de.md
@@ -0,0 +1,28 @@
+**--- Tag 9: Alles in einer einzigen Nacht ---**
+
+Jedes Jahr schafft es der Weihnachtsmann, alle seine Geschenke in einer einzigen Nacht auszuliefern.
+
+In diesem Jahr hat er jedoch einige neue Orte zu besuchen; seine Elfen haben ihm die Entfernungen zwischen jedem Paar
+von Orten mitgeteilt. Er kann an zwei beliebigen (unterschiedlichen) Orten beginnen und enden, muss aber jeden Ort genau
+einmal besuchen. Was ist die kürzeste Entfernung, die er zurücklegen kann, um dies zu erreichen?
+
+```
+Nehmen wir zum Beispiel die folgenden Entfernungen an:
+
+London nach Dublin = 464
+von London nach Belfast = 518
+Dublin nach Belfast = 141
+
+Die möglichen Strecken sind also:
+
+Dublin -> London -> Belfast = 982
+London -> Dublin -> Belfast = 605
+London -> Belfast -> Dublin = 659
+Dublin -> Belfast -> London = 659
+Belfast -> Dublin -> London = 605
+Belfast -> London -> Dublin = 982
+
+Die kürzeste Strecke ist London -> Dublin -> Belfast = 605, also lautet die Antwort in diesem Beispiel 605.
+```
+
+Wie groß ist die Entfernung der kürzesten Strecke?
