f39c79a017f5d60a95ed5bfca3c203c5346dee72
Rico Kunz <ricci1710@gmail.com>
2023-01-06 22:38:13 +0100

    day 04 start

--- src/day09/Day09.test.js
+++ src/day09/Day09.test.js
@@ -9,6 +9,27 @@ describe('Test Class Day09', () => {
     expect(lifeDay).toBeDefined();
   });
   test('calcPartOne with demo data', () => {
+    const permutator = (inputArr) => {
+      let result = [];
+
+      const permute = (arr, m = []) => {
+        if (arr.length === 0) {
+          result.push(m)
+        } else {
+          for (let i = 0; i < arr.length; i++) {
+            let curr = arr.slice();
+            let next = curr.splice(i, 1);
+            permute(curr.slice(), m.concat(next))
+          }
+        }
+      }
+
+      permute(inputArr)
+
+      return result;
+    }
+    const x = permutator(['London', 'Dublin', 'Belfast']);
+    console.info(x);
     const result = demoDay.calcPartOne();
     expect(result).toEqual(-1);
   });
