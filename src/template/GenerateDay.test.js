34e0fd93c19ab1690dd7c69809bb395bcfa18c1a
Rico Kunz <rico.kunz@leutek.de>
2023-01-10 12:59:42 +0100

    Automation zur Erstellung eines Tages implementiert

--- src/template/GenerateDay.test.js
+++ src/template/GenerateDay.test.js
@@ -1,8 +1,128 @@
 import DayGenerator from "./DayGenerator";
 
 describe('Generate a Day', () => {
-  test('generate', async () => {
+  test.skip('generate day01', async () => {
+    const dayGenerator = new DayGenerator('01');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day02', async () => {
+    const dayGenerator = new DayGenerator('02');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day03', async () => {
+    const dayGenerator = new DayGenerator('03');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day04', async () => {
+    const dayGenerator = new DayGenerator('04');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day05', async () => {
+    const dayGenerator = new DayGenerator('05');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day06', async () => {
+    const dayGenerator = new DayGenerator('06');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day07', async () => {
+    const dayGenerator = new DayGenerator('07');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day08', async () => {
+    const dayGenerator = new DayGenerator('08');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day09', async () => {
+    const dayGenerator = new DayGenerator('09');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day10', async () => {
+    const dayGenerator = new DayGenerator('10');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day11', async () => {
+    const dayGenerator = new DayGenerator('11');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day12', async () => {
+    const dayGenerator = new DayGenerator('12');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day13', async () => {
+    const dayGenerator = new DayGenerator('13');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day14', async () => {
+    const dayGenerator = new DayGenerator('14');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day15', async () => {
+    const dayGenerator = new DayGenerator('15');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day16', async () => {
+    const dayGenerator = new DayGenerator('16');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day17', async () => {
+    const dayGenerator = new DayGenerator('17');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day18', async () => {
+    const dayGenerator = new DayGenerator('18');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day19', async () => {
+    const dayGenerator = new DayGenerator('19');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day20', async () => {
+    const dayGenerator = new DayGenerator('20');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day21', async () => {
+    const dayGenerator = new DayGenerator('21');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day22', async () => {
     const dayGenerator = new DayGenerator('22');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day23', async () => {
+    const dayGenerator = new DayGenerator('23');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day24', async () => {
+    const dayGenerator = new DayGenerator('24');
+    const success = await dayGenerator.generate();
+    expect(success).toBeTruthy();
+  });
+  test.skip('generate day25', async () => {
+    const dayGenerator = new DayGenerator('25');
     const success = await dayGenerator.generate();
     expect(success).toBeTruthy();
   });
