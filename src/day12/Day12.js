6682e2fd03846da291db3e4207553e11750adc6e
ricci1710 <ricci1710@gmail.com>
2023-01-10 18:08:25 +0100

    day 12 finished

--- src/day12/Day12.js
+++ src/day12/Day12.js
@@ -1,4 +1,4 @@
-import Day from "../base/Day";
+import Day from '../base/Day';
 
 class Day12 extends Day {
   constructor(dayNumber, loadDemoData) {
@@ -26,66 +26,8 @@ class Day12 extends Day {
     return Day12.findAllNumbers(this.storeData.toString());
   }
 
-  // calcPartTwo() {
-  //   let result = 0;
-  //   const data = JSON.parse(this.storeData);
-  //   const test = (value, result) => {
-  //     value.forEach((item) => {
-  //       if (Array.isArray(item)) {
-  //         test(item);
-  //       } else if (typeof item === 'object') {
-  //         const values = Object.values(item);
-  //         const valueString = values.toString();
-  //         if (valueString.indexOf('red') === -1) {
-  //           for (let idx = 0; idx < values.length; idx += 1) {
-  //             let objectValue = values[idx].toString();
-  //             const num = parseInt(objectValue, 10);
-  //             if (!isNaN(num))
-  //               result += num;
-  //           }
-  //         }
-  //       }
-  //       console.log(item);
-  //     });
-  //     return result;
-  //   }
-  //
-  //   test(data);
-  //   return result;
-  // }
-
   calcPartTwo() {
     const data = JSON.parse(this.storeData);
-    // const test = (value) => {
-    //   const removeIndices = [];
-    //   for (let removeIdx = 0; removeIdx < value.length; removeIdx += 1) {
-    //     // value.forEach((item) => {
-    //     const item = value[removeIdx];
-    //     if (Array.isArray(item)) {
-    //       test(item);
-    //     }
-    //     else if (typeof item === 'object') {
-    //       const values = Object.values(item);
-    //       const valueString = values.toString();
-    //       if (valueString.indexOf('red') >= 0) {
-    //         removeIndices.push(removeIdx);
-    //       }
-    //       else {
-    //         values.forEach(objValue => {
-    //           if (typeof objValue === 'object')
-    //             test(objValue);
-    //           console.log(objValue);
-    //         });
-    //       }
-    //     }
-    //     else if (typeof item === 'string' && item === 'red') {
-    //       removeIndices.push(removeIdx);
-    //     }
-    //   }
-    //   removeIndices.forEach(delIdx => value.splice(delIdx, 1));
-    //   console.log(data);
-    // }
-
     const recursiveArray = (array) => {
       const removeIndices = [];
       for (let idx = 0; idx < array.length; idx += 1) {
@@ -114,16 +56,6 @@ class Day12 extends Day {
           }
       }
       return false;
-      // for (const value of Object.values(objValue)) {
-      //   if (value === 'red') {
-      //     objValue = {};
-      //     break;
-      //   } else if (Array.isArray(value))
-      //     recursiveArray(value);
-      //   else if (typeof value === 'object')
-      //     objValue = recursiveObject(value);
-      // }
-      // return objValue;
     };
 
     const test = (value) => {
