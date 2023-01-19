1dd3cedcaa3da4685e45ffdd4aa4eefbe35acd6b
Rico Kunz <rico.kunz@leutek.de>
2023-01-19 19:33:34 +0100

    day 24 / 25 start

--- src/day25/Day25.js
+++ src/day25/Day25.js
@@ -4,9 +4,19 @@ class Day25 extends Day {
   firstCode = 20151125;
   factor = 252533;
   divisor = 33554393;
+  playground = [];
+  destination = { row: 0, column: 0 };
 
   constructor(dayNumber, loadDemoData) {
     super(dayNumber, loadDemoData);
+
+    const data = this.storeData[0];
+    const rowIndex = data.indexOf('row ');
+    const rowIndexEnd = data.indexOf(',', rowIndex);
+    this.destination.row = parseInt(data.substring(rowIndex + 4, rowIndexEnd));
+
+    const columnIndex = this.storeData[0].indexOf('column ');
+    this.destination.column = parseInt(data.substring(columnIndex + 7, data.length - 1));
   }
 
   static getNextCode(value, factor, divisor) {
